2fd983c88ff8901a099fa9761d7207c7954a1c9b
Rado Jedro <radoslaw.jedro@gmail.com>
2020-02-25 20:17:47 +0000

    Update collection

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const ObjectId = require('mongodb').ObjectId;
 
 router.get('/products', (req, res) => {
-  req.db.collection('/products').find().toArray((err, data) => {
+  req.db.collection('products').find().toArray((err, data) => {
     if(err) res.status(500).json( {message: err});
     else res.json(data);
   });
