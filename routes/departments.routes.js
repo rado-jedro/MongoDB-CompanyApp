1401e6af40fa269eac4cb81d7c9baa757d53979d
Rado Jedro <radoslaw.jedro@gmail.com>
2020-02-26 20:18:45 +0000

    Update res.json

--- routes/departments.routes.js
+++ routes/departments.routes.js
@@ -51,7 +51,7 @@ router.put('/departments/:id', async (req, res) => {
     if (dep) {
       dep.name = name;
       await dep.save();
-      res.json({ message: `Element ${dep} has been updated` });
+      res.json(dep);
     } else res.status(404).json({ message: 'Not found...' });
   } catch (err) {
     res.status(500).json(err);
@@ -63,7 +63,7 @@ router.delete('/departments/:id', async (req, res) => {
     const dep = await Department.findById(req.params.id);
     if (dep) {
       await Department.deleteOne({ _id: req.params.id });
-      res.json({ message: `Element ${dep} has been deleted` });
+      res.json(dep);
     } else res.status(404).json({ message: 'Not found...' });
   } catch (err) {
     res.status(500).json(err);
